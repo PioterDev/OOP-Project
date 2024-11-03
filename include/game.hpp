94ea2d1491ba5f0d22917d201f4f59fd2eeaf42a
PioterDev <pimikotr@gmail.com>
2024-11-03 22:14:22 +0100

    GameRenderer & game changes

--- include/game.hpp
+++ include/game.hpp
@@ -12,7 +12,8 @@
 #define GAME_HPP
 
 #include "Game/GameRenderer.hpp"
-#include "Game/GameRegistry.hpp"
+#include "Game/Main/MainRegistry.hpp"
+#include "Game/World.hpp"
 #include "program.hpp"
 #include "resources.hpp"
 #include "input.hpp"
@@ -21,16 +22,26 @@ class Game : public Program {
     friend class InputHandler;
 
     private:
-        static GameRegistry registry;
+        static MainRegistry registry;
         static GameRenderer renderer;
         static InputHandler inputHandler;
+
+        World world;
+
     public:
+        Game() : world(1) /* TODO: change this later */ {
+            
+        }
         ~Game();
-        static GameRegistry& getRegistry() { return registry; }
+        static MainRegistry& getRegistry() { return registry; }
         static GameRenderer& getRenderer() { return renderer; }
         static InputHandler& getInputHandler() { return inputHandler; }
         NoDiscard Status init();
         void run();
+
+        static uint64_t getTimeSinceLastFrame() { return renderer.getTimeSinceLastFrame(); }
+
+        World& getWorld() { return this->world; }
 };
 
 #endif
