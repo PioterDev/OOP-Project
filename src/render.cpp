7096a7cf888a3be669bf895b5bfe50048988817a
PioterDev <pimikotr@gmail.com>
2024-12-31 00:18:03 +0100

    Drawability of arcs, circles and ellipses

--- src/render.cpp
+++ src/render.cpp
@@ -1,7 +1,127 @@
 #include "Game/GameRenderer.hpp"
 #include "Game/Main/Game.hpp"
+#include "Math.hpp"
 
 constexpr double sizeOfBlockTexture = 64.0;
+
+/**
+ * @brief Draws an arc on the current rendering target.
+ * Used for drawing circular and elliptical arcs, as well as
+ * circles and ellipses.
+ * To draw a circular arc, set `rX` equal to `rY`.
+ * 
+ * @param renderer rendering context
+ * @param center point around which the arc will be drawn
+ * @param rX horizontal radius of the arc
+ * @param rY vertical radius of the arc
+ * @param start start of the arc in degrees, automatically clamped to [0, 360] degrees
+ * @param end end of the arc in degrees, automatically clamped to [0, 360] degrees;
+ * draws counterclockwise if `end < start`
+ * @param centerColor color of the central point
+ * @param outerColor color of outer points; if `outerColor` is different than
+ * `centerColor` a gradient is applied between them
+ * @param srcTex (optional) texture to copy pixel data from, if
+ * `centerColor` and `outerColor` are not white, a mask from these colors is applied
+ * @param srcTexCenter (optional) center point of the part of `srcTex`
+ * to copy
+ */
+void DrawArc(
+    SDL_Renderer* renderer, PointF center, float rX, float rY,
+    double start, double end, Color centerColor, Color outerColor,
+    SDL_Texture* srcTex, PointF srcTexCenter
+) {
+    float inv_w = 0.0f, inv_h = 0.0f;
+    if(srcTex) {
+        int texWidth, texHeight;
+        if(SDL_QueryTexture(srcTex, nullptr, nullptr, &texWidth, &texHeight)) return;
+        inv_w = 1.0f / (float)texWidth; inv_h = 1.0f / (float)texHeight;
+        if(srcTexCenter.x + rX > (float)texWidth || srcTexCenter.x - rX < 0.0f) return; //out-of-bounds
+        if(srcTexCenter.y + rY > (float)texHeight || srcTexCenter.y - rY < 0.0f) return; //out-of-bounds
+    }
+    constexpr double step = 1.0;
+
+    while(start < 0.0) { start += 360.0; }
+    while(start > 360.0) { start -= 360.0; }
+
+    while(end < 0.0) { end += 360.0; }
+    while(end > 360.0) { end -= 360.0; }
+
+    double angle = end - start;
+    if(angle < 0.0) angle += 360.0;
+    
+    u32 steps = (u32)(floor(abs(angle) * 10.0) * 0.1 * (1.0 / step));
+    SDL_Vertex vertices[(size_t)(360.0 / step) + 2];
+    int indices[3 * (size_t)(360.0 / step)];
+
+    vertices[0].position = {center.x, center.y};
+    vertices[0].color = *(SDL_Color*)&centerColor;
+    if(srcTex) vertices[0].tex_coord = {srcTexCenter.x * inv_w, srcTexCenter.y * inv_h};
+    {
+        u32 i;
+        float t;
+        if(start > end) {
+            for(i = 1, t = (float)(end + 360.0); i < steps + 2; i++, t -= (float)step) {
+                Vector2f off = {
+                    rX * Math::SinTable<(size_t)(90.0 / step), float>::at(t),
+                    -rY * Math::CosTable<(size_t)(90.0 / step), float>::at(t)
+                };
+                vertices[i].position = {
+                    center.x + off.x,
+                    center.y + off.y
+                };
+                vertices[i].color = *(SDL_Color*)&outerColor;
+                if(srcTex) {
+                    vertices[i].tex_coord = {
+                        (srcTexCenter.x + off.x) * inv_w,
+                        (srcTexCenter.y + off.y) * inv_h,
+                    };
+                }
+            }
+        }
+        else {
+            for(i = 1, t = (float)start; i < steps + 2; i++, t += (float)step) {
+                Vector2f off = {
+                    rX * Math::SinTable<(size_t)(90.0 / step), float>::at(t),
+                    -rY * Math::CosTable<(size_t)(90.0 / step), float>::at(t)
+                };
+                vertices[i].position = {
+                    center.x + off.x,
+                    center.y + off.y
+                };
+                vertices[i].color = *(SDL_Color*)&outerColor;
+                if(srcTex) {
+                    vertices[i].tex_coord = {
+                        (srcTexCenter.x + off.x) * inv_w,
+                        (srcTexCenter.y + off.y) * inv_h,
+                    };
+                }
+            }
+        }
+    }
+    for(u32 i = 0, j = 1; i < 3 * steps; i += 3, j++) {
+        indices[i] = 0;
+        indices[i + 1] = j;
+        indices[i + 2] = j + 1;
+    }
+
+    SDL_RenderGeometry(renderer, srcTex, vertices, steps + 2, indices, 3 * steps);
+}
+
+ForceInline void DrawCircle(
+    SDL_Renderer* renderer, PointF center, float r,
+    Color centerColor, Color outerColor,
+    SDL_Texture* srcTex, PointF srcTexCenter
+) {
+    DrawArc(renderer, center, r, r, 0.0, 360.0, centerColor, outerColor, srcTex, srcTexCenter);
+}
+
+ForceInline void DrawEllipse(
+    SDL_Renderer* renderer, PointF center, float rX, float rY,
+    Color centerColor, Color outerColor,
+    SDL_Texture* srcTex, PointF srcTexCenter
+) {
+    DrawArc(renderer, center, rX, rY, 0.0, 360.0, centerColor, outerColor, srcTex, srcTexCenter);
+}
 
 void GameRenderer::renderInPlace(Game& game) {
     Size windowSize = game.getWindowSize();
@@ -96,6 +216,28 @@ void GameRenderer::renderInPlace(Game& game) {
             SDL_SetTextureBlendMode(texture, SDL_BLENDMODE_NONE);
         }
     });
+
+    ///    Section for render testing     ///
+    static double start = 0.0, end = 270.0;
+    SDL_Texture* tex = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 320, 320);
+
+    Size s = Program::getResourceManager().getTextureOriginalSize(MainRegistry::gregTextureIndex);
+    PointF p = {(float)(s.width / 2), (float)(s.height / 2)};
+    SDL_SetTextureBlendMode(tex, SDL_BLENDMODE_BLEND);
+    SDL_SetRenderTarget(renderer, tex);
+    DrawArc(
+        renderer, {160.0f, 160.0f}, 160.0f, 160.0f, start, end, Colors::YELLOW, Colors::MAGENTA,
+        Program::getResourceManager().getTexture(MainRegistry::gregTextureIndex), p
+    );
+
+    // DrawCircle(renderer, {250.0f, 250.0f}, 125.0f, {255,255,255,0}, {255,255,255,0}, nullptr, {});
+    SDL_SetRenderTarget(renderer, nullptr);
+    SDL_Rect rr = {0, 0, 320, 320};
+    SDL_RenderCopy(renderer, tex, nullptr, &rr);
+    SDL_DestroyTexture(tex);
+    start += 1.0;
+    end += 1.0;
+    /// End of section for render testing ///
 
     //TODO: formalize text rendering into a separate entity
     //This can get complicated though as adding anything
